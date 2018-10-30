e2efb343e7077b3030b78581177cb528c2e8d772
Sergii81 <sezik1@gmail.com>
2018-10-30 21:39:53 +0200

    kendo ui start

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,92 +5,124 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Test</title>
+        
+        <link rel="stylesheet" href="styles/kendo.common.min.css" />
+        <link rel="stylesheet" href="styles/kendo.default.min.css" />
+        <link rel="stylesheet" href="styles/kendo.default.mobile.min.css" />
+        <link rel="stylesheet" href="styles/kendo.material.min.css" />
+
+        <script src="js/jquery.min.js"></script>
+        <script src="js/kendo.all.min.js"></script>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
             .content {
-                text-align: center;
+                margin: 0 auto;
+                border: 1px solid #000;
             }
+                #fieldlist {
+                    margin: 0 auto;
+                    padding: 0;
+                }
 
-            .title {
-                font-size: 84px;
-            }
+                #fieldlist li {
+                    list-style: none;
+                    padding-bottom: .7em;
+                    text-align: left;
+                }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+                #fieldlist label {
+                    display: block;
+                    padding-bottom: .3em;
+                    font-weight: bold;
+                    text-transform: uppercase;
+                    font-size: 12px;
+                    color: #444;
+                }
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+                #fieldlist li .k-widget:not(.k-tooltip),
+                #fieldlist li .k-textbox {
+                    margin: 0 5px 5px 0;
+                }
+
+                span.k-widget.k-tooltip-validation {
+                    display: inline-block;
+                    width: 160px;
+                    text-align: left;
+                    border: 0;
+                    padding: 0;
+                    margin: 0;
+                    background: none;
+                    box-shadow: none;
+                    color: red;
+                }
+                
+                .k-tooltip-validation .k-warning {
+                    display: none;
+                }
+            </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
+        <div class="content">            
+                <form id="employeeForm" data-role="validator" novalidate="novalidate">
+                    <ul id="fieldlist">
+                        <li>
+                            <label for="FirstName">First Name:</label>
+                            <input type="text" class="k-textbox" name="FirstName" id="FirstName" required="required" />
+                        </li> 
+                        <li>
+                            <label for="LastName">Last Name:</label>
+                            <input type="text" class="k-textbox" name="LastName" id="LastName" required="required" />
+                        </li>   
+                        <li>
+                            <label for="HireDate">Hire Date:</label>
+                            <input type="text" data-role='datepicker' id="HireDate" name="HireDate" data-type="date" required="required"  />
+                            <span data-for='HireDate' class='k-invalid-msg'></span>
+                        </li> 
+                        <li>
+                            <label for="RetireDate">Retire Date:</label>
+                            <input type="text" data-role='datepicker' id ="RetireDate" data-type="date" name="RetireDate" data-greaterdate-field="HireDate" data-greaterdate-msg='Retire date should be after Hire date' />
+                            <span data-for='RetireDate' class='k-invalid-msg'></span>
+                        </li> 
+                        <li>
+                             <button type="button" class="k-primary" data-role="button" data-click='save'>Save</button>
+                        </li>       
+                    </ul>
+                </form>
+            </div>                      
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+            <script type="text/javascript">
+                $(function () {
+                    var container = $("#employeeForm");
+                    kendo.init(container);
+                    container.kendoValidator({
+                        rules: {
+                            greaterdate: function (input) {
+                                if (input.is("[data-greaterdate-msg]") && input.val() != "") {                                    
+                                    var date = kendo.parseDate(input.val()),
+                                        otherDate = kendo.parseDate($("[name='" + input.data("greaterdateField") + "']").val());
+                                    return otherDate == null || otherDate.getTime() < date.getTime();
+                                }
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+                                return true;
+                            }
+                        }
+                    });
+                });
+
+                function save(e) {
+                    var validator = $("#employeeForm").data("kendoValidator");
+                    if (validator.validate()) {
+                        alert("Employee Saved");
+                    }
+                }
+            
+            </script>    
+        
         </div>
     </body>
 </html>
